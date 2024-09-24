327847c7f364745100027d516bb77d91b7f11a5e
LeAh11-rgb <leahw113004@gmail.com>
2024-09-24 17:40:58 +0100

    Add files via upload

---
+++ Website/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('next-button');
+
+    nextButton.addEventListener('click', function() {
+        window.location.href = 'consent.html'; // Navigate to the consent page
+    });
+
+});
